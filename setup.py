7877dd641252a8b118ba8e42442e237a123ee37b
Eliecer Garcia Rey <erey@freepik.com>
2023-07-07 19:37:16 +0200

    fix: update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(
-    name='egarciarey1981-math',
+    name='egarciarey1981_math',
     version='0.1.5',
     description='My first Pip Package',
     packages=['math'],
