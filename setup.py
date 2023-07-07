84452b2f4e943438d2c04144a5421e079c875efc
Eliecer Garcia Rey <erey@freepik.com>
2023-07-07 19:20:22 +0200

    fix(setup.py): rename py_modules

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='egarciarey1981-math',
-    version='0.1.2',
+    version='0.1.4',
     description='My first Pip Package',
-    py_modules=['math'],
+    py_modules=['egarciarey1981_math'],
 )
